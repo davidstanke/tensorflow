23796d8e3c8ec9f4c25b9fad7b61deaf2fe34269
David Stanke <davidstanke@gmail.com>
2018-07-19 01:23:38 +0000

    refine Maven

--- next2018/demo.sh
+++ next2018/demo.sh
@@ -1,3 +1,1 @@
-IMAGE=3.jpg
-
-java -cp ../bazel-bin/tensorflow/java/libtensorflow.jar -Djava.library.path=../bazel-bin/tensorflow/java -jar ../bazel-bin/next2018/tfjavademo_deploy.jar
+java -jar ../bazel-bin/next2018/tfjavademo_deploy.jar
